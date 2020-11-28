130fbd60a15095f9abf696d2d09c305b14c01b38
hafizhpratama <hafizhpratama99@gmail.com>
2020-11-28 15:46:07 +0800

    menambahkan fitur tambah pemesanan dan mengurangi kuota, mengubah data pemesanan dan menghapus data pemesanan.

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -15,7 +15,7 @@ namespace ServiceReservasi
 		string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi);//method proses input
 
 		[OperationContract]
-		string editPemesanan(string IDPemesanan, string NamaCustomer);
+		string editPemesanan(string IDPemesanan, string NamaCustomer, string No_telpon);
 
 		[OperationContract]
 		string deletePemesanan(string IDPemesanan);
